ecc6534dcd0eecf566b8610d881e2d0ccb188c65
bibitor1 <evgeniishogolev93@gmail.com>
2023-02-22 17:28:32 +0300

    Исправил ошибки

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,17 @@
-function checkLength(string, number) {
-  return string.length <= number;
-}
+const isLong = (string, number) => string.length <= number;
+isLong('Я ничего не понимаю', 10);
 
-function checkPalindrom (string) {
+const isPalindrom = (string) => {
   string = string.toLowerCase().replace(/ /g,'');
   return string === string.split('').reverse().join('');
-}
+};
+isPalindrom('ТоПот');
 
-function getNumbers(string) {
+const getNumbers = (string) => {
   if (typeof string === 'number') {
     return string;
   }
+
   let str = '';
   for (let i = 0; i < string.length; i++) {
     if (!Number.isNaN(parseInt(string[i], 10))) {
@@ -18,12 +19,15 @@ function getNumbers(string) {
     }
   }
   return parseInt(str, 10);
-}
+};
+getNumbers('2023 год');
 
-function addSmth (string, minl, adds) {
-  if (string.length >= minl) {
+const addSmth = (string, minLength, piece) => {
+  if (string.length >= minLength) {
     return string;
   }
-  let newadds = minl - string.length;
-  return adds.slice(0, newadds % adds.length) + adds.repeat(newadds / adds.length) + string;
-}
+
+  const newPiece = minLength - string.length;
+  return piece.slice(0, newPiece % piece.length) + piece.repeat(newPiece / piece.length) + string;
+};
+addSmth('q', 4, 'we');
