1476ed30ae0d837bcc7ade360ecc66f2d07e9fd5
bibitor1 <evgeniishogolev93@gmail.com>
2023-02-22 11:21:45 +0300

    Написали функции

--- js/functions.js
+++ js/functions.js
@@ -1,33 +1,29 @@
 function checkLength(string, number) {
-  return (string.length <= number) ? true : false;
+  return string.length <= number;
 }
-console.log(checkLength('Я ничего не понимаю', 20));
 
 function checkPalindrom (string) {
   string = string.toLowerCase().replace(/ /g,'');
-  return (string == string.split('').reverse().join('')) ? true : false;
+  return string === string.split('').reverse().join('');
 }
-console.log(checkPalindrom('Лёша на полке клопа нашёл '));
 
 function getNumbers(string) {
   if (typeof string === 'number') {
-      return string;
-      }
+    return string;
+  }
   let str = '';
-  for (i = 0; i < string.length; i++) {
-      if (!Number.isNaN(parseInt(string[i], 10))) {
+  for (let i = 0; i < string.length; i++) {
+    if (!Number.isNaN(parseInt(string[i], 10))) {
       str += string[i];
-      }
+    }
   }
   return parseInt(str, 10);
 }
-console.log(getNumbers('2023 год'));
 
 function addSmth (string, minl, adds) {
   if (string.length >= minl) {
-      return string;
+    return string;
   }
-      let newadds = minl - string.length;
-      return adds.slice(0, newadds % adds.length) + adds.repeat(newadds / adds.length) + string;
+  let newadds = minl - string.length;
+  return adds.slice(0, newadds % adds.length) + adds.repeat(newadds / adds.length) + string;
 }
-console.log(addSmth ('q', 4, 'werty'))
